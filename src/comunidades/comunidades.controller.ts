f9b32c65a877c2caf96f5922c23f5223227317cf
Elias Bernardo <ebmm01@gmail.com>
2021-09-16 13:48:21 -0300

    [#14] implementing community CRUD operations
    Co-authored-by: Leonardo Barreiros <leossb36@gmail.com>

--- src/comunidades/comunidades.controller.ts
+++ src/comunidades/comunidades.controller.ts
@@ -1,13 +1,38 @@
 import { Controller } from '@nestjs/common';
 import { MessagePattern, Payload } from '@nestjs/microservices';
 import { ComunidadesService } from './comunidades.service';
+import { CreateCommunityDto } from './dto/createCommunity.dto';
+import { UpdateCommunityDto } from './dto/updateCommunity.dto';
 
 @Controller()
 export class ComunidadesController {
   constructor(private readonly comunidadesService: ComunidadesService) {}
 
-  @MessagePattern('createComunidade')
-  create(@Payload() id: string) {
-    return this.comunidadesService.create(id);
+  @MessagePattern('createCommunity')
+  async create(@Payload() createCommunity: CreateCommunityDto) {
+    const { id } = await this.comunidadesService.create(createCommunity);
+
+    return id;
+  }
+
+  @MessagePattern('updateCommunity')
+  async update(@Payload() updateCommunity: UpdateCommunityDto) {
+    const { id } = await this.comunidadesService.update(updateCommunity);
+
+    return id;
+  }
+
+  @MessagePattern('getCommunity')
+  async get(@Payload() id: string) {
+    const community = await this.comunidadesService.getById(id);
+
+    return community.toJSON();
+  }
+
+  @MessagePattern('deleteCommunity')
+  async delete(@Payload() id: string) {
+    await this.comunidadesService.destroy(id);
+
+    return true;
   }
 }
--- src/comunidades/comunidades.service.ts
+++ src/comunidades/comunidades.service.ts
@@ -1,8 +1,54 @@
-import { Injectable } from '@nestjs/common';
+import { HttpStatus, Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { MicrosserviceException } from 'src/commons/exceptions/MicrosserviceException';
+import { CreateCommunityDto } from './dto/createCommunity.dto';
+import { UpdateCommunityDto } from './dto/updateCommunity.dto';
+import { Community, CommunityDocument } from './entities/comunidade.schema';
 
 @Injectable()
 export class ComunidadesService {
-  create(id: string) {
-    return `This action adds a new comunidade with id ${id}`;
+  constructor(
+    @InjectModel(Community.name)
+    private communityModel: Model<CommunityDocument>,
+  ) {}
+
+  async create(communityData: CreateCommunityDto) {
+    const community = new this.communityModel(communityData);
+
+    try {
+      return community.save();
+    } catch (err) {
+      throw new MicrosserviceException(err.message, HttpStatus.BAD_REQUEST);
+    }
+  }
+
+  async update(updateCommunity: UpdateCommunityDto) {
+    const community = await this.getById(updateCommunity.id);
+
+    if (updateCommunity.name) community.name = updateCommunity.name;
+    if (updateCommunity.description)
+      community.description = updateCommunity.description;
+    if (updateCommunity.imageUrl) community.imageUrl = updateCommunity.imageUrl;
+
+    return community.save();
+  }
+
+  async destroy(id: string) {
+    const community = await this.getById(id);
+
+    return community.delete();
+  }
+
+  async getById(id: string) {
+    const community = await this.communityModel.findById(id);
+
+    if (!community)
+      throw new MicrosserviceException(
+        'Comunidade não encontrada',
+        HttpStatus.NOT_FOUND,
+      );
+
+    return community;
   }
 }
