055eb03682649ac659872ac1170acd961d849f69
Guilherme Guy <guilhermeguy349@gmail.com>
2021-09-21 15:09:42 -0300

    [#17] test - Add unit tests to comunity controller
    Co-authored-by: Lorrany Azevedo <lorrany90@gmail.com>

--- test/comunidades/comunidades.controller.spec.ts
+++ test/comunidades/comunidades.controller.spec.ts
@@ -109,4 +109,155 @@ describe('ComunidadesController', () => {
 
     expect(await controller.delete('123')).toBe(true);
   });
+
+  it('should add a user to a community', async () => {
+    const userRelation: any = {
+      userId: '1234',
+      communityId: '4321',
+    };
+
+    const module = await customModule({
+      addUser: () => ({
+        id: '999',
+        ...userRelation,
+      }),
+    });
+
+    controller = module.get<ComunidadesController>(ComunidadesController);
+
+    expect(await controller.addUser(userRelation)).toStrictEqual({
+      communityId: '4321',
+      id: '999',
+      userId: '1234',
+    });
+  });
+
+  it('should get all users of a community', async () => {
+    const userRelations: any = [
+      {
+        id: '1',
+        userId: '1234',
+        communityId: '4321',
+      },
+      {
+        id: '2',
+        userId: '1235',
+        communityId: '4321',
+      },
+      {
+        id: '3',
+        userId: '1236',
+        communityId: '4321',
+      },
+    ];
+
+    const module = await customModule({
+      getUsers: (communityId: string) => {
+        return userRelations.filter((element) => {
+          return communityId === element.communityId;
+        });
+      },
+    });
+
+    controller = module.get<ComunidadesController>(ComunidadesController);
+
+    expect(await controller.getUsers('4321')).toStrictEqual(userRelations);
+  });
+
+  it('should remove users of a community', async () => {
+    const userRelations: any = [
+      {
+        id: '1',
+        userId: '1234',
+        communityId: '4321',
+      },
+      {
+        id: '2',
+        userId: '1235',
+        communityId: '4321',
+      },
+      {
+        id: '3',
+        userId: '1236',
+        communityId: '4321',
+      },
+    ];
+
+    const userRelationToRemove: any = {
+      userId: '1234',
+      communityId: '4321',
+    };
+
+    const module = await customModule({
+      removeUser: (userRelation) => {
+        userRelations.splice(
+          userRelations.indexOf(
+            userRelations.filter((element) => {
+              return (
+                userRelation.communityId === element.communityId &&
+                userRelation.userId === element.userId
+              );
+            }),
+          ),
+          1,
+        );
+      },
+    });
+
+    controller = module.get<ComunidadesController>(ComunidadesController);
+
+    await controller.removeUser(userRelationToRemove);
+
+    expect(userRelations).not.toContain({
+      id: '1',
+      userId: '1234',
+      communityId: '4321',
+    });
+  });
+
+  it('should get a user from a community', async () => {
+    const userRelations: any = [
+      {
+        id: '1',
+        userId: '1234',
+        communityId: '4321',
+      },
+      {
+        id: '2',
+        userId: '1235',
+        communityId: '4321',
+      },
+      {
+        id: '3',
+        userId: '1236',
+        communityId: '4321',
+      },
+    ];
+
+    const userRelationToFind: any = {
+      userId: '1234',
+      communityId: '4321',
+    };
+
+    const module = await customModule({
+      getCommunityUser: (userRelation) => {
+        return userRelations.filter((element) => {
+          return (
+            userRelation.communityId === element.communityId &&
+            userRelation.userId === element.userId
+          );
+        })[0];
+      },
+    });
+
+    controller = module.get<ComunidadesController>(ComunidadesController);
+
+    expect(await controller.getCommunityUser(userRelationToFind)).toStrictEqual(
+      {
+        id: '1',
+        userId: '1234',
+        communityId: '4321',
+      },
+    );
+  });
 });
